7c984d93a8dc68734d59ff90e31bda74bb1f3772
Ayberk Tosun <ayberk.tosun@gmail.com>
2023-10-16 22:31:52 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/opt/homebrew/bin/fish
+
+## Change directory.
+echo -n "Changing directory into `~/Code/ayberkt.github.io`... "
+pushd ~/Code/ayberkt.github.io; and echo " OK"; or exit 1
+echo "Current working directory is `$(pwd)`"
+
+## Remove the old forest.
+echo -n "Removing `mathematical-forest`..."
+rm -rf mathematical-forest; and echo " OK"; or exit 1
+
+## Copy the new forest.
+echo -n "Copying the last generated forest..."
+cp -r ~/Code/mathematical-forest/output mathematical-forest; and echo " OK"; or exit 1
+
+## Verify that files are the same.
+if diff -r mathematical-forest ~/Code/mathematical-forest/output
+    echo "Successfully copied the forest."
+else
+    echo "Something went wrong! Two copies are not the same." && exit 1
+end
+
+## Commit to git.
+echo -n "Adding to git..."
+git add mathematical-forest; echo " OK"; or exit 1
+echo -n "Committing... "
+git commit -m "Update forest [$(date "+%Y-%m-%dT%H:%M:%S")]"; and echo " OK"; or exit 1
+
+## Push.
+echo "Running `git push`"
+git push
+
+popd
